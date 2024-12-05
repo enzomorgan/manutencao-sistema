d242349937d7aabb80c954f2be009de50f835428
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2024-12-05 17:16:25 -0300

    correções de erros e atualizações

--- condominio/models.py
+++ condominio/models.py
@@ -6,14 +6,14 @@ class CasaModel(models.Model):
 
     numero = models.IntegerField()
     bloco = models.IntegerField()
-    quantidade_quartos = models.IntegerField()
-    quantidade_banheiros = models.IntegerField()
-    area_lazer = models.BooleanField()
-    garagem = models.BooleanField()
-    disponivel = models.BooleanField()
+    quantidade_quartos = models.IntegerField(default=0)
+    quantidade_banheiros = models.IntegerField(default=0)
+    area_lazer = models.BooleanField(default=False)
+    garagem = models.BooleanField(default=False)
+    disponivel = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Cala {self.numero}'
+        return f'Casa {self.numero}'
     
     class Meta:
         verbose_name = "Casa"
@@ -21,11 +21,12 @@ class CasaModel(models.Model):
 
 class CondominioModel(models.Model):
     casa_numero = models.IntegerField()
-    endereco = models.CharField(max_length=250)
+    endereco = models.CharField(max_length=250, default="Endereço não especificado")
+
 
     def __str__(self):
-        return f'Condominio - Casa[{self.sala_numero}] - [{self.hora_inicio}]'
+        return f'Condominio - Casa[{self.casa_numero}] - [{self.endereco}]'
     
     class Meta:
-        verbose_name = "Reserva"
-        verbose_name_plural = "Reservas"
+        verbose_name = "Condomínio"
+        verbose_name_plural = "Condomínios"
--- condominio/tests.py
+++ condominio/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 
-from condominios.models import CasaModel
+from condominio.models import CasaModel
 
 # Create your tests here.
 
